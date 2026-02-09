3c8620268944bbf6bc7ca9350d50bdf8b0f8e8e2
SuspiciousApplePie <andreiusita6@gmail.com>
2026-02-09 23:06:12 +0800

    Render dialog when clicking add task btn

--- src/controller.js
+++ src/controller.js
@@ -2,12 +2,14 @@ import { Storage } from "./storage";
 import { NavBar } from "./ui";
 import { TaskDisplay } from "./ui";
 import { clearContent } from "./ui";
+import { AddTaskDialog } from "./ui";
 
 export class Controller {
     constructor() {
         this.main = document.querySelector(".main");
         this.nav = new NavBar("Menu");
         this.task = new TaskDisplay();
+        this.addTaskDialog = new AddTaskDialog();
         this.setUpListener();
     }
 
@@ -18,7 +20,7 @@ export class Controller {
                 this.nav.renderNavBar(Storage.readProjectNames());
                 this.task.renderTask(Storage.readAllTask(Storage.getProject(e.target.dataset.id)));
             } else if (e.target.id === "add-task-btn") {
-                console.log("Add task");
+                this.addTaskDialog.renderAddTaskDialog();
             }
         })
     };
